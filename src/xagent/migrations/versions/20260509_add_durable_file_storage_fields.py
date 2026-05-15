0b89ea80aaa41a92d7dafe1db0315bb2ad95fac0
bsbds <bsb4ds@gmail.com>
2026-05-15 15:32:43 +0800

    fix: alembic version

--- src/xagent/migrations/versions/20260509_add_durable_file_storage_fields.py
+++ src/xagent/migrations/versions/20260509_add_durable_file_storage_fields.py
@@ -5,7 +5,7 @@ from alembic import op
 from sqlalchemy.engine.reflection import Inspector
 
 revision: str = "20260509_add_durable_file_storage_fields"
-down_revision: Union[str, None] = "7f4d2c9a1b58"
+down_revision: Union[str, None] = "20260514_add_user_template_relations"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
